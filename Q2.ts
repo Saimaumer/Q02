fd2b642272b1b89f5498f75049cd4f6dd8b1fd83
saima umer <142655523+Saimaumer@users.noreply.github.com>
2024-03-06 01:09:08 +0500

    Add files via upload

---
+++ Q2.ts
@@ -0,0 +1,6 @@
+//QUESTION NO2
+//   Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+let personName:string = 'hello saima,';
+let message:string = '"would you like to learn some typescript today?"';
+console.log(`${personName} ${message}`);
